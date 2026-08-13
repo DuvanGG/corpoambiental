3ca291f1d57977d5c4e1efb02fa700b4d4198279
DuvanGG <duvanga@hotmail.es>
2026-08-13 09:51:49 -0500

    Se agrega redes sociales, se agrega iconos, se agrega funcionalidad de envio de correo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,20 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: 'https://duvangg.github.io',
-  base: '/corpoambiental',
+
+  site: 'https://corpo-ambiental.vercel.app/',
+  base: '/',
+
   integrations: [
     sitemap(),
   ],
+
+  adapter: vercel(),
 });
